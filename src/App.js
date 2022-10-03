3db9838d31d6e9fbfe05d98a4c1614c18a5b355b
Masse04 <massamba084@gmail.com>
2022-10-03 06:40:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+import Boots from './Composant/header';
+function App() {
+  return (
+    <div className="App">
+      <Boots/>
+    </div>
+  );
+}
+
+export default App;
